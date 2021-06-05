01a63395a71fc1929f4089c1c67351d2b195407e
Akash <akkuchauhan2610@gmail.com>
2021-06-05 12:37:25 +0530

    Aakash

--- notify.py
+++ notify.py
@@ -1,3 +1,8 @@
 import requests
-x = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110096&date=29-06-2021")
-print(x.text)
+import json
+Pincode=int(input("Enter the pincode in ypur area:"))
+date=(input("Enter the date:"))
+x = requests.get(f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={Pincode}&date={date}")
+raw_json=x.json()
+for i in range(len(raw_json['centers'])):
+    print(raw_json['centers'][i]["name"])
